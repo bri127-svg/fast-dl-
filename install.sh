8d1c3ad22e50467b810a742a12a68f991648384e
briancarlos.dev <usabrifreites@gmail.com>
2026-02-04 00:00:57 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -1397,9 +1397,6 @@ wings_remove() {
   sleep 2
 }
 
-# ==============================
-# MENÚ WINGS
-# ==============================
 instalar_wings() {
   clear
   echo "================================================"
@@ -1408,8 +1405,7 @@ instalar_wings() {
   echo ""
   echo "  [1] Instalar Wings (Normal / IP)"
   echo "  [2] Instalar Wings con Subdominio + SSL"
-  echo "  [3] Eliminar Wings"
-  echo ""
+  echo "  [3] borra Wings"
   echo "  [0] Volver"
   echo ""
   read -rp "Selecciona una opción: " WINGS_OPTION
@@ -1417,12 +1413,7 @@ instalar_wings() {
   case "$WINGS_OPTION" in
     1) SSL_MODE=0 ;;
     2) SSL_MODE=1 ;;
-    3)
-      read -rp "¿Seguro que deseas eliminar Wings? (s/N): " confirm
-      [[ "$confirm" =~ ^[SsYy]$ ]] || { log_info "Operación cancelada."; sleep 1; return; }
-      wings_remove
-      return
-      ;;
+    3) wings_remove;;
     0) return ;;
     *) log_warn "Opción inválida"; sleep 1; return ;;
   esac
@@ -1447,6 +1438,11 @@ instalar_wings() {
   else
     log_info "Docker ya instalado"
   fi
+
+  # ==============================
+  # DIRECTORIOS
+  # ==============================
+  mkdir -p /etc/pterodactyl /var/lib/pterodactyl
 
   # ==============================
   # DESCARGA WINGS
@@ -1517,7 +1513,7 @@ EOF
 
     certbot --nginx -d "$WINGS_DOMAIN" --non-interactive --agree-tos -m admin@"$WINGS_DOMAIN"
   fi
- mkdir -p /etc/pterodactyl /var/lib/pterodactyl
+
   # ==============================
   # TOKEN
   # ==============================
